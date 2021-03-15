4c55d78f64a7a5796b863ee384e5778d1e9983cf
Geovani Pereira <geovanimac@hotmail.com>
2021-03-15 18:08:15 -0300

    adicionado o CORS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import {Router} from 'express';
 
 import employeesRouter from './employees.routes';
+import cors from 'cors'
+
+
 
 const routes = Router();
-
+routes.use(cors());
 routes.use('/employees', employeesRouter);
 
 export default routes;
